df880d91b4c1b19b5228ee112e987f651a5030ed
Jonathan Côté <55141844+snakeeyes1023@users.noreply.github.com>
2022-10-21 10:02:31 -0400

    router middleware

--- src/router/index.js
+++ src/router/index.js
@@ -3,26 +3,31 @@ import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
+    public : true,
     path: '/',
     name: 'home',
     component: HomeView
   },
   {
+    public : true,
     path: '/about',
     name: 'about',
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   },
   {
+    public : true,
     path: "/login",
     name: "Login",
     component: () => import(/* webpackChunkName: "about" */ '../views/auths/Login.vue')  
   },
   {
+    public : true,
     path: "/register",
     name: "Register",
     component: () => import(/* webpackChunkName: "about" */ '../views/auths/Register.vue')
   },
   {
+    public : false,
     path: "/unit",
     name: "Unit",
     component: () => import(/* webpackChunkName: "about" */ '../views/units/UnitView.vue')
@@ -30,9 +35,24 @@ const routes = [
 
 ]
 
+
+
 const router = createRouter({
   history: createWebHashHistory(),
   routes
 })
+
+router.beforeEach((to, from, next) => {
+
+  const publicPages = routes.filter(route => route.public).map(route => route.path);
+  const authRequired = !publicPages.includes(to.path);
+  const loggedIn = localStorage.getItem('user');
+
+  if (authRequired && !loggedIn) {
+    next('/login');
+  } else {
+    next();
+  }
+});
 
 export default router
