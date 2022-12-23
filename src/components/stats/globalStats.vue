39919f771a02af63ef903b2c6c71cec34fa5b027
Jonathan Côté <cotejonathan638@gmail.com>
2022-12-23 07:37:09 -0500

    Ajout d'une statistique

--- src/components/stats/globalStats.vue
+++ src/components/stats/globalStats.vue
@@ -1,6 +1,6 @@
 <template>
     <v-card class="mx-auto" max-width="480" min-width="400">
-        <v-card-item title="Total capacitée">
+        <v-card-item :title="$t('percentageInUse')">
         </v-card-item>
 
         <v-card-text class="py-0">
@@ -13,53 +13,27 @@
 
         <div class="d-flex py-3 justify-space-between">
             <v-list-item density="compact" prepend-icon="mdi-account-group-outline">
-                <v-list-item-subtitle>{{vacationData.totalUsed}}</v-list-item-subtitle>
+                <v-list-item-subtitle>{{ vacationData.totalUsed }}</v-list-item-subtitle>
             </v-list-item>
 
             <v-list-item density="compact" prepend-icon="mdi-cube-outline">
-                <v-list-item-subtitle>{{vacationData.totalUnits}}</v-list-item-subtitle>
+                <v-list-item-subtitle>{{ vacationData.totalUnits }}</v-list-item-subtitle>
             </v-list-item>
         </div>
-
-        <v-divider></v-divider>
-
-        <v-card-actions>
-            <v-btn @click="refreshData" prepend-icon="mdi-refresh">
-                Rafraîchir les données
-            </v-btn>
-        </v-card-actions>
     </v-card>
 
 
     <v-card class="mx-auto" max-width="480" min-width="400">
-        <v-card-item title="Total par status">
+        <v-card-item :title="$t('averageReservationDuration')">
         </v-card-item>
 
         <v-card-text class="py-0">
             <v-row align="center" no-gutters>
                 <v-col class="text-h2" cols="12">
-                    {{ vacationData.pourcentageVacation }}%
+                    {{ totalDays }} {{ $t("days") }}
                 </v-col>
             </v-row>
         </v-card-text>
-
-        <div class="d-flex py-3 justify-space-between">
-            <v-list-item density="compact" prepend-icon="mdi-account-group-outline">
-                <v-list-item-subtitle>{{vacationData.totalUsed}}</v-list-item-subtitle>
-            </v-list-item>
-
-            <v-list-item density="compact" prepend-icon="mdi-cube-outline">
-                <v-list-item-subtitle>{{vacationData.totalUnits}}</v-list-item-subtitle>
-            </v-list-item>
-        </div>
-
-        <v-divider></v-divider>
-
-        <v-card-actions>
-            <v-btn @click="refreshData" prepend-icon="mdi-refresh">
-                Rafraîchir les données
-            </v-btn>
-        </v-card-actions>
     </v-card>
 </template>
 
@@ -70,12 +44,15 @@ export default {
     name: 'GlobalStats',
     data() {
         return {
-            vacationData : {
+            vacationData: {
                 totalUnits: 0,
                 totalUsed: 0,
                 pourcentageVacation: 0
+            },
+            averageDays: {
+                days: 0
             }
-            
+
         }
     },
     mounted() {
@@ -84,14 +61,32 @@ export default {
     methods: {
         refreshData() {
             StatsDataService.getTotalVacation()
-            .then(response => {
-                if (response.status == 200) {
-                    this.vacationData = response.data;                    
-                }
-            }).catch(e => {
-                console.log(e);
-            });
+                .then(response => {
+                    if (response.status == 200) {
+                        this.vacationData = response.data;
+                    }
+                }).catch(e => {
+                    console.log(e);
+                });
+
+
+            StatsDataService.getAverageReservationDays()
+                .then(response => {
+                    if (response.status == 200) {
+                        this.averageDays = response.data;
+                    }
+                }).catch(e => {
+                    console.log(e);
+                });
         },
+    },
+    computed: {
+        totalDays: {
+            // getter
+            get() {
+                return this.averageDays.days == 0 ? this.$t("noData") : ~~this.averageDays.days;
+            },
+        }
     }
 }
 </script>
--- src/services/StatsDataService.js
+++ src/services/StatsDataService.js
@@ -2,8 +2,17 @@ import http from "../http-common";
 
 class StatsDataService {
 
+    /**
+     * Le taux de réservation du jour
+     * 
+     * @returns 
+     */
     getTotalVacation() {
         return http.get(`/Stats/TotalVacation`);
+    }
+
+    getAverageReservationDays() {
+        return http.get(`/Stats/AverageReservationTotalDays`);
     }
 }
 
